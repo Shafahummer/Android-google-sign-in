8eb0c137a29f89b95bf9655b34d93ba02bc3b3fa
Shafahummer <44742741+Shafahummer@users.noreply.github.com>
2019-01-09 11:53:53 +0530

    android google login

---
+++ SecondActivity.java
@@ -0,0 +1,50 @@
+package shait.firegoogleloginapp;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SecondActivity extends AppCompatActivity {
+Button button;
+FirebaseAuth mAuth;
+FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        button =findViewById(R.id.log_out);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser()==null){
+                  Intent intent = new Intent(SecondActivity.this,MainActivity.class);
+                  startActivity(intent);
+                }
+
+            }
+        };
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                mAuth.signOut();
+
+            }
+        });
+    }
+}
